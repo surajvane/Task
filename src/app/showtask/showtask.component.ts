09999d8cd434e274d82c67fc4f33882856ad7684
surajvane <84263912+surajvane@users.noreply.github.com>
2021-09-18 08:32:18 +0530

    Update showtask.component.ts

--- src/app/showtask/showtask.component.ts
+++ src/app/showtask/showtask.component.ts
@@ -9,59 +9,103 @@ import { CrudService } from '../services/crud.service';
 })
 export class ShowtaskComponent implements OnInit {
   public result:any;
-
+  public progress:any=[];
+  public complete:any=[];
 
   constructor(private crud:CrudService) { }
-  fetchTask(){
+  openTask(abc:any){
   
-  this.crud.fetchData('task').subscribe((res:any)=>{
-    this.result=res;
-  
-  },
-  (err)=>{
-console.log(err);
-  })
+    this.crud.fetchData('Task'+abc).subscribe((res:any)=>{
+      this.result=res;
+     
+    
+    },
+    (err)=>{
+  console.log(err);
+    })
+  }
+  progressTask(abc:any){
+    
+    this.crud.fetchData('Task'+abc).subscribe((res:any)=>{
+      this.progress=res;
+     
+    
+    },
+    (err)=>{
+  console.log(err);
+    })
+  }
+  completeTask(abc:any){
+    
+    this.crud.fetchData('Task'+abc).subscribe((res:any)=>{
+      this.complete=res;
+     
+    
+    },
+    (err)=>{
+  console.log(err);
+    })
   }
   ngOnInit(): void { 
-   
-    this.fetchTask();
-    
-
-
+    this.openTask("?status=1");
+    this.progressTask("?status=2");
+    this.completeTask("?status=3");
   }
-  onItemDrag(e: any) {
+  onItemDrag(Dragitem:any) {
    
-  var ob=e.target.attributes.for.value.split('#'); 
+  var obj=Dragitem.target.attributes.for.value.split('#'); 
   console.log("fromdrag")
-  console.log(ob); 
-  localStorage.setItem("title",ob[0]);
-  localStorage.setItem("description",ob[1]);
-  localStorage.setItem("status",ob[2]);
-  localStorage.setItem("datetime",ob[3]);
-  localStorage.setItem("id",ob[4]);
+  console.log(obj); 
+  localStorage.setItem("title",obj[0]);
+  localStorage.setItem("description",obj[1]);
+  localStorage.setItem("status",obj[2]);
+  localStorage.setItem("datetime",obj[3]);
+  localStorage.setItem("id",obj[4]);
  
-  console.log(e.target);
-
-
+  console.log(Dragitem.target);
   }
   
-  public progress:any=[];
   
-  onItemDrop(e: any) { 
-   
-    var t=this.progress.push(e.dragData);
-   
-    // 
-    console.log("drg data "+e.dragData);
+  onItemDrop(Dropitem:any) { 
+    var t=this.progress.push(Dropitem.dragData);
+    console.log("drg data "+Dropitem.dragData);
     console.log(this.progress)
-   
     console.log(this.result);
+
    
-    this.result.splice(this.result.indexOf(e.dragData),1);
+    this.result.splice(this.result.indexOf(Dropitem.dragData),1);
       console.log(this.result);
 
+      localStorage.setItem("status","2");
+      var obj={
+        title:localStorage.getItem("title"),
+        description:localStorage.getItem("description"),
+        status:localStorage.getItem("status"),
+        datetime:localStorage.getItem("datetime"),
+        id:localStorage.getItem("id")
+      }
+      console.log(obj)
+      console.log("collection name");
+      console.log(`Task?id=${obj.id}`,obj);
+      
+      this.crud.putData(`Task/${obj.id}`,obj).subscribe(
+        (response:any)=>{
+      console.log(response);
+    },
+     (err:any)=>{
+      console.log(err);
 
-    localStorage.setItem("status","2");
+    })   
+}
+  
+  onItemDropComplete(DropComplete:any) {
+    this.complete.push(DropComplete.dragData);
+    var position=this.result.indexOf(DropComplete.dragData);
+    this.result.splice(position,1);
+    console.log(this.result);
+    console.log(DropComplete.dragData);
+    console.log(this.complete);
+    localStorage.setItem("status","3");
     var obj={
       title:localStorage.getItem("title"),
       description:localStorage.getItem("description"),
@@ -70,51 +114,12 @@ console.log(err);
       id:localStorage.getItem("id")
     }
     console.log(obj)
-    console.log("collection name");
-    console.log(`task?id=${obj.id}`,obj);
     
-    this.crud.putData("task/"+obj.id,obj).subscribe(
-      (response:any)=>{
-    console.log(response);
-   
-    
-  
+    this.crud.putData("Task/"+obj.id,obj).subscribe((res:any)=>{
+        console.log(res);
     },(error:any)=>{
 
-    }
-    )
-    
-  }
-  public complete:any=[];
-  onItemDropComplete(e:any) {
-    this.complete.push(e.dragData);
-    var position=this.result.indexOf(e.dragData);
-     this.result.splice(position,1);
-      console.log(this.result);
-    console.log(e.dragData);
-    console.log(this.complete);
-    localStorage.setItem("status","3");
-    var obj={
-      title:localStorage.getItem("title"),
-    
-    description:localStorage.getItem("description"),
-    status:localStorage.getItem("status"),
-
-    datetime:localStorage.getItem("datetime"),
-    id:localStorage.getItem("id")
-    }
-    console.log(obj)
-    //console.log("collection name");
-    //console.log(`task?id=${obj.id}`,obj);
-    
-    this.crud.putData("task/"+obj.id,obj).subscribe(
-      (response:any)=>{
-    console.log(response);
-    
-    },(error:any)=>{
-
-    }
-    )
+    })
   } 
  
 }
